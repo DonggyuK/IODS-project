edb8dcc0f194a9d110d6cfd736588f014ae68119
Donggyu Kam <donggyu.kam@helsinki.fi>
2018-12-04 15:51:35 +0200

    data

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,7 @@
+BPRS<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep="", header=TRUE)
+RATS<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep="\t", header=T)
+
+
+library(dplyr)
+library(tidyr)
+
